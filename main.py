3af161d8ebabc8050e7deb077b956d99e0e1ec36
Siyavuya Mankanku <siphokwesi1@outlook.com>
2023-03-23 00:13:45 +0000

    second version

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 from art import logo
-print(logo)
 
-end_of_calculation = False
+def add(n1, n2):
+    return n1 + n2
 
-def calculator(f_number, operation, second_number):
-    if operation == "+":
-        return f_number + second_number
-    elif operation == "-":
-        return f_number - second_number
-    elif operation == "*":
-        return f_number * second_number
-    elif operation == "/":
-        return f_number / second_number
-    
+def subtract(n1, n2):
+    return n1 - n2
 
-while end_of_calculation != True:
-    first_number = float(input("What's the first number?:"))
-    choice_of_operation = input("Pick an operation: ")
-    next_number = float(input("What's the next number?: "))
-    output = calculator(first_number, choice_of_operation, next_number)
-    print(f"{first_number} {choice_of_operation} {next_number} = {output}")
-    continue_calculating = input(f"Type 'y' to continue or type 'n' to start a new calculation")
-    if continue_calculating == "y":
-        first_number = output
-    elif continue_calculating == "n":
-        end_of_calculation = True
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    print(logo)
+
+num1 = float(input("What's the first number?: "))
+for symbol in operations:
+    print(symbol)
+should_continue = True
+
+while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+        num1 = answer
+    else:
+        should_continue = False
+        clear()
+        calculator()
+
+calculator()
